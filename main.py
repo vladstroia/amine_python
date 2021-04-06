7e54ebea59c6211b63f449e1faee985eb978bd39
vladstroia <vlad.stroia@gmail.com>
2021-04-06 20:50:02 +0300

    Ads

--- main.py
+++ main.py
@@ -12,14 +12,21 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO Inputs VALUES (%s,%s,%s,%s,%s, %s)"
+
+# sql querry string
+sql = "INSERT INTO Inputs VALUES (%s, %s,%s,%s,%s,%s, %s)"
 #  val = (1,2,3,4,5,6)
 
-
+# sql = "INSERT INTO Inputs VALUES (%f, %f,%f,%f,%f,%f, %f)"
 
 
 while True:
+    # 6 valori random intr-o lista
     val = [round(random.uniform(20, 60), 2) for i in range (6)]
+    
+    #adaugam timestamp 
+    val =  [time.time()] + val
+    print(val)
     mycursor.execute(sql, val)
     mydb.commit()
     time.sleep( 1 )
