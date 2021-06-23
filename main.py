e077023a399ed220b62e35f8d64e6998df1519c3
vladstroia <vlad.stroia@gmail.com>
2021-06-23 06:06:03 +0200

    integrated into main.py the code for reading the analog voltages (not yet tested)

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import time
-import random
+
+# import random
 
 import mysql.connector
 
+#pt ads1115 (placa care citeste senzori de temp)
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+
+import adafruit_ads1x15.ads1115 as ADS
+
+from adafruit_ads1x15.analog_in import AnalogIn
+
+ads1 = ADS.ADS1115(i2c)                   #prima placa ads1115
+ads2 = ADS.ADS1115(i2c, address=0x49)     #pentru a doua placa
+
+
+
+#conexiunea la baza de date
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-#  password="caracter",
+ password="caracter",
   database="Amine"
 )
 
@@ -31,10 +47,45 @@ sql_rezistente = "SELECT * FROM Rezistente ORDER BY NumberId DESC LIMIT 1;"
 #     PRIMARY KEY (NumberId)
 # );
 
+
+
+
+#ST1
+# ads1_chan_0 = AnalogIn(ads1, ADS.P0)    #canalul 0 de pe placa 1
+# #ST2
+# ads1_chan_1 = AnalogIn(ads1, ADS.P1)    #canalul 1 de pe placa 1
+# ads1_chan_2 = AnalogIn(ads1, ADS.P2)    #
+# ads1_chan_3 = AnalogIn(ads1, ADS.P3)    #
+# ads2_chan_0 = AnalogIn(ads1, ADS.P0)    #canalul 0 de pe placa 2
+# #ST6
+# ads2_chan_1 = AnalogIn(ads1, ADS.P1)    #canalul 1 de pe placa 2
+
+
+
+# am grupat ce era mai sus intr-un vector
+channels = [
+          AnalogIn(ads1, ADS.P0),[AnalogIn(ads1, ADS.P1),
+          [AnalogIn(ads1, ADS.P2),[AnalogIn(ads1, ADS.P3),
+          [AnalogIn(ads2, ADS.P0),[AnalogIn(ads2, ADS.P1),
+          ]
+
+
+
+
+
+
 while True:
     # 6 valori random intr-o lista
-    val = [round(random.uniform(20, 60), 2) for i in range (6)]
+    # val = [round(random.uniform(20, 60), 2) for i in range (6)]
     
+    #val este lista cu cele 6 temperaturi
+    #prin channel.voltage citim tensiunea inregistrata pe fiecare canal si apoi facem calculele necesare pt a gasi temperatura  
+    val = [200*(channel.voltage - 0.8)/3 for channel in channels]
+
+
+
+
+
     #adaugam timestamp 
     mytime = time.asctime( time.localtime(time.time()) )
     val =  [mytime] + val
--- read_from_sensor.py
+++ read_from_sensor.py
@@ -7,14 +7,35 @@ import adafruit_ads1x15.ads1115 as ADS
 
 from adafruit_ads1x15.analog_in import AnalogIn
 
-ads = ADS.ADS1115(i2c)
+ads1 = ADS.ADS1115(i2c)
+ads2 = ADS.ADS1115(i2c, address=0x49)     #pentru a doua placa
 
-chan = AnalogIn(ads, ADS.P0)
-#ads.gain = 2/3
+#ST1
+# ads1_chan_0 = AnalogIn(ads1, ADS.P0)    #canalul 0 de pe placa 1
+# #ST2
+# ads1_chan_1 = AnalogIn(ads1, ADS.P1)    #canalul 1 de pe placa 1
+# ads1_chan_2 = AnalogIn(ads1, ADS.P2)    #
+# ads1_chan_3 = AnalogIn(ads1, ADS.P3)    #
+# ads2_chan_0 = AnalogIn(ads1, ADS.P0)    #canalul 0 de pe placa 2
+# #ST6
+# ads2_chan_1 = AnalogIn(ads1, ADS.P1)    #canalul 1 de pe placa 2
+
+
+
+# am grupat ce era mai sus intr-un vector
+channels = [
+          AnalogIn(ads1, ADS.P0),[AnalogIn(ads1, ADS.P1),
+          [AnalogIn(ads1, ADS.P2),[AnalogIn(ads1, ADS.P3),
+          [AnalogIn(ads2, ADS.P0),[AnalogIn(ads2, ADS.P1),
+          ]
+
+
+
 
 while True:
-    volt = chan.voltage
-    temp = 200*(volt-0.8)/3
-    print( volt, temp)
-    time.sleep(1) 
+    # volt_st1 = ads1_chan_1.voltage
+    # temp_st1 = 200*(volt_volt_st1-0.8)/3
+    # print( volt_st1, temp_st1)
+    # time.sleep(1) 
 
+    val = [200*(channels[i].voltage - 0.8)/3 for i in range(channels)]
